d746672e31d83487586817079b32707dd9fa0805
Your name <you@example.com>
2023-04-16 23:24:56 +0200

    challenge

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 function lineOn() {
     document.getElementById("access").style.borderBottom= "none";
-
   }
   function lineOut() {
-    document.getElementById("access").style.borderBottom= "rgb(0,255,0)";
-    
+    document.getElementById("access").style.borderBottom= "2px solid rgb(0, 128, 0)";
   }
   function onLine() {
     document.getElementById("line").style.borderBottom= "none";
   }
   function outLine() {
-    document.getElementById("line").style.borderBottom= "underline";
-    document.getElementById("line").style.textDecorationColor ="green"
-      "rgb(0, 255, 0)";
-    document.getElementById("line").style.cursor = "pointer";
+    document.getElementById("line").style.borderBottom= "3px solid rgb(0, 128, 0)";
   }
   function numbers() {
     document.getElementById("all").style.display = "flex";
+  }
+  function number(){
+    document.getElementById("hover").style.backgroundColor="rgb(0,0,255)"
+  }
+  function numbe(){
+    document.getElementById("hover").style.backgroundColor="#3aff9e"
+  }
+  function pic(){
+    this.setAttribute("src","image-hero.webp")
+  }
+  function picOut(){
+    this.setAttribute("src","image-founder.webp")
   }
